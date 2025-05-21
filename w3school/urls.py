31c073b38e5d5c68292323b5a68315a2b884c0c6
Mehdi900000 <Mehdi900000@email.com>
2025-05-21 08:58:21 +0330

    add w3school+base config

---
+++ w3school/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import messageview
+
+
+urlpatterns =[
+    path('',messageview.as_view(),name='message')
+]
+
+
---
+++ w3school/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+class messageview (TemplateView):
+    template_name ='1.html'
+
+# Create your views here.
