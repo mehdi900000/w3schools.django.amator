04f91e91e6eccae6f2820b754e6031fb49326c21
Mehdi900000 <Mehdi900000@email.com>
2025-05-28 16:34:14 +0330

    add crispy

--- config/settings.py
+++ config/settings.py
@@ -42,7 +42,13 @@ INSTALLED_APPS = [
     "w3school",
     "post",
     "accounts",
+#     third party
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 AUTH_USER_MODEL = 'accounts.CustomUser'
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
